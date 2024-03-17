c5b7709180278abd14bbb9f091d0da75562269c7
wimonsiri3x45 <145572730+wimonsiri3x45@users.noreply.github.com>
2024-03-17 19:44:07 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+/* Navbar start */
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/* Navbar end */
+
+/* Search Bar start */
+let dropdownBtnText = document.getElementById("drop-text");
+let span = document.getElementById("span");
+let icon = document.getElementById("icon");
+let list = document.getElementById("list");
+let input = document.getElementById("search-input");
+let listItems = document.querySelectorAll(".dropdown-list-item");
+
+dropdownBtnText.onclick = function () {
+  list.classList.toggle("show");
+  icon.style.rotate = "-180deg";
+};
+
+window.onclick = function (e) {
+  if (
+    e.target.id !== "drop-text" &&
+    e.target.id !== "icon" &&
+    e.target.id !== "span"
+  ) {
+    list.classList.remove("show");
+    icon.style.rotate = "0deg";
+  }
+};
+
+for (item of listItems) {
+  item.onclick = function (e) {
+    span.innerText = e.target.innerText;
+    if (e.target.innerText == "Everything") {
+      input.placeholder = "Search Anything...";
+    } else {
+      input.placeholder = "Search in " + e.target.innerText + "...";
+    }
+  };
+}
+/* Search Bar ends */
